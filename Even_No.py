b829e36ac3fb0ee2d57e8cb1e27d7fa0bfb36eb9
vbkulkarni <73324381+vbkulkarni@users.noreply.github.com>
2022-03-08 23:28:04 +0530

    added else statement

--- Even_No.py
+++ Even_No.py
@@ -4,3 +4,6 @@ num = int (input (“Enter any number to test whether it is odd or even: “)
 if (num % 2) == 0:
 
               print (“The number is even”)
+else
+	print("The number is odd")
+              
